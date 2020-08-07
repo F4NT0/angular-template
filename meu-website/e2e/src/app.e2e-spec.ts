e444e7b589783510badba31613cec7a59139d03f
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-08-06 22:32:55 -0300

    Instalação correta do Angular

--- meu-website/e2e/src/app.e2e-spec.ts
+++ meu-website/e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('minecraft-itens App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('minecraft-itens App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getTitleText()).toEqual('Welcome to meu-website!');
   });
 });
